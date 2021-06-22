ae90e3b0524c2045b3620e72bf6681228964538a
Igor Polousov <igorpolousov@gmail.com>
2021-06-22 14:18:26 +0300

    Made all nice to haves

--- Guess the flag milestone 1/DetailedViewController.swift
+++ Guess the flag milestone 1/DetailedViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class DetailedViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    var selctedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selctedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
+        title = selctedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Guess the flag milestone 1/TableViewController.swift
+++ Guess the flag milestone 1/TableViewController.swift
@@ -14,10 +14,11 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Flag viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
-        
         let path = Bundle.main.resourcePath!
-        
         let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items {
@@ -39,7 +40,15 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
         cell.textLabel?.text = pictures[indexPath.row]
+        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailedViewController {
+            vc.selctedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
 }
